d84f7f67d24f4b2940615e4431f1140198db1897
AmirHdm <hadjmassouda@gmail.com>
2023-04-15 00:02:36 +0200

    UPDATE keys External class

--- src/KeysExternal.java
+++ src/KeysExternal.java
@@ -3,7 +3,6 @@ public class KeysExternal {
 	public static String TEST_DATA_PATH = "C:\\Users\\Amir\\eclipse-workspace\\TestNGSample\\testDataEx.xlsx";
 	public static final String FIREFOX_PATH = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
 	
-	public static final int WAIT_TIME = 30; // Delay time to wait the website
 	// launch completely
 	public static final String BASE_URL = "http://www.demo.guru99.com/";
 	
