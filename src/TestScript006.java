bb4d59d2db05beea182f26811f92b6f2fe8905da
AmirHdm <hadjmassouda@gmail.com>
2023-04-15 01:28:25 +0200

    Add new class TestScript 006

--- src/TestScript006.java
+++ src/TestScript006.java
@@ -1,59 +1,53 @@
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
-
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+public class TestScript006 {
+	@Test(dataProvider = "loginData")
+	public void testCase05(String username, String password) throws Exception {
 
-
-public class TestScript004 {
- 
-	
-	
-	@DataProvider(name = "GuruTest")
-	public Object[][] testData() throws Exception {
-		int tab []=FunctExternal.InitXLSX(KeysExternal.TEST_DATA_PATH);
-		return FunctExternal.readXLSXFile(KeysExternal.TEST_DATA_PATH, tab[0], tab[1]);
-	}
-	
-	
-	
-	@Test(dataProvider = "GuruTest")
-	public void testCase04(String username, String password) throws Exception {
-		//Sitting the webdriver parameters
 		System.setProperty("webdriver.firefox.driver","C:\\firefox\\geckodriver.exe");
 		Thread.sleep(2000);	
     	WebDriver driver = new FirefoxDriver();
 		String baseUrl = KeysExternal.BASE_URL;
 		driver.get(baseUrl + "/V4/");
 		Thread.sleep(2000);
-		String actualTitle;
 		String actualBoxMsg;
 		driver.findElement(By.name("uid")).clear();
 		driver.findElement(By.name("uid")).sendKeys(username);
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys(password);
 		driver.findElement(By.name("btnLogin")).click();
-		Thread.sleep(5000);
-	    try{ 
-	    
+		Thread.sleep(2000);
+	    try{
 	       	Alert alt = driver.switchTo().alert();
 			actualBoxMsg = alt.getText(); 
 			alt.accept();				
 			assertEquals(actualBoxMsg,KeysExternal.EXPECT_ERROR);
-		}    
-	    catch (NoAlertPresentException Ex){ 
-	    	actualTitle = driver.getTitle();
-	    	assertEquals(actualTitle,KeysExternal.EXPECT_TITLE);
-	    	
-	    	
+		} 
+	    catch (NoAlertPresentException Ex){
+	    	String pageText = driver.findElement(By.xpath("/html/body/table/tbody")).getText();
+			assertTrue(pageText.contains(username.substring(0, 4)));
+			assertTrue(pageText.contains(username.substring(4)));
+			Thread.sleep(2000);	
         }
 	    driver.manage().deleteAllCookies();
-		Thread.sleep(2000);
-	    driver.quit();
-	    
+	    driver.close();
 	}
+	
+	@DataProvider(name = "loginData")
+	public Object[][] TestingData() throws Exception {
+		
+		return new Object[][] {
+            { KeysExternal.USER_NAME, KeysExternal.USER_PASSWORD },
+            { "invalid", "valid" },
+            { "valid", "invalid" },
+            { "invalid", "invalid" },
+			{ KeysExternal.USER_NAME, KeysExternal.USER_PASSWORD }
+    };
+    }
 	
 }
