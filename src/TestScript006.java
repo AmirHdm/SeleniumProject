6498ef2cac9fa7dd5699289eca14f4d03bfa0e3a
AmirHdm <hadjmassouda@gmail.com>
2023-04-15 02:19:44 +0200

    Unit Test for the user access and taking a screenshot of the user id after verification Screenshot for the happy flow : Test access + Take a screenshot

--- src/TestScript006.java
+++ src/TestScript006.java
@@ -1,9 +1,13 @@
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
 
 public class TestScript006 {
 	@Test(dataProvider = "loginData")
@@ -32,7 +36,20 @@ public class TestScript006 {
 	    	String pageText = driver.findElement(By.xpath("/html/body/table/tbody")).getText();
 			assertTrue(pageText.contains(username.substring(0, 4)));
 			assertTrue(pageText.contains(username.substring(4)));
-			Thread.sleep(2000);	
+			Thread.sleep(2000);
+			// Take the screenshot
+	        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+	        // Specify the path and filename of the screenshot
+	        String screenshotPath = "C:\\Users\\Amir\\eclipse-workspace\\TestNGSample\\test-output\\screenshot.png";
+
+	        try {
+	            // Copy the screenshot file to the specified location
+	            FileHandler.copy(screenshotFile, new File(screenshotPath));
+	            System.out.println("Screenshot saved to " + screenshotPath);
+	        } catch (IOException e) {
+	            System.out.println("Error saving screenshot: " + e.getMessage());
+	        }
         }
 	    driver.manage().deleteAllCookies();
 	    driver.close();
@@ -43,10 +60,12 @@ public class TestScript006 {
 		
 		return new Object[][] {
             { KeysExternal.USER_NAME, KeysExternal.USER_PASSWORD },
+            /*
             { "invalid", "valid" },
             { "valid", "invalid" },
             { "invalid", "invalid" },
 			{ KeysExternal.USER_NAME, KeysExternal.USER_PASSWORD }
+			*/
     };
     }
 	
